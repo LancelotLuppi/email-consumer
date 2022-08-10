480fedbad310aa9867da159975efdf36e66fcf97
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-08-10 17:35:20 -0300

    Adicionando projeto de email consumer

---
+++ src/test/java/br/com/vemser/petshop/emailconsumer/EmailConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vemser.petshop.emailconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
